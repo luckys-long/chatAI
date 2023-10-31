209bda699ba640612b9be7998d250a9dd770c14a
zhanglong <zhanglong2@shsany.com>
2023-10-31 18:59:03 +0800

    优化

--- src/pages/home/components/chatList.vue
+++ src/pages/home/components/chatList.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="chat-list">
-  <el-scrollbar height="calc(100vh - 34.8vh)">
-    <p v-for="item in 20" :key="item" effect="light">
-      {{ item }}
-    </p>
-  </el-scrollbar>
-</div>
+    <el-scrollbar height="calc(100vh - 34.8vh)">
+      <div class="chat-item" v-for="item in 20">聊天{{ item }}</div>
+    </el-scrollbar>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ElScrollbar } from "element-plus";
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .scrollbar-demo-item {
   display: flex;
   align-items: center;
@@ -22,9 +20,20 @@ import { ElScrollbar } from "element-plus";
   text-align: center;
   border-radius: 4px;
   background: var(--el-color-primary-light-9);
-  color: var(--el-color-primary);
 }
-.chat-list{
+.chat-list {
   margin-top: 10px;
+  padding: 10px;
+  .chat-item {
+    margin-top: 5px;
+    width: 96%;
+    padding: 5px;
+    border-radius: 10px;
+    font-size: 14px;
+    font-weight: 200;
+    letter-spacing: 1px;
+    text-decoration: none;
+    background: rgba($color: #f8f4f4, $alpha: 0.6);
+  }
 }
 </style>
